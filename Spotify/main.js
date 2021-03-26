0e3ec34072dc1133d907845d8694730c94944bb2
Damilola Aiyetigbo <damilolaaiyetigbo@Damilolas-MacBook-Pro.local>
2021-03-25 22:51:18 -0400

    Commit for Sprint 3

--- Spotify/main.js
+++ Spotify/main.js
@@ -1,19 +1,53 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
 const url = require('url');
 const path = require('path');
 
+let win; 
+
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 400,
     height: 320,
+    icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       nodeIntegration: true
     }
   })
 
-  win.loadFile('index.html')
+  //win.loadFile('index.html')
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //Open the DevTools
+  //win.webContents.openDevTools()
+
+  //Emitted when the window is closed.
+  win.on('closed', () => {
+
+    win = null
+  })
+
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {label: 'File'},
+        {label: 'View'},
+        {label: 'Exit'}
+      ]
+    }
+  ])
+
+  Menu.setApplicationMenu(menu);
 }
 
+
+//method will be called when electron has finished initialization
+//and is ready to create browser windows
 app.whenReady().then(createWindow)
 
 //app.on('window-all-closed', () => {
